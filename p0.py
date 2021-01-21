104bb6bbbd31f229550111b9067d1d7d922cfaf5
Matthew Boyle <63613741+QPolaris@users.noreply.github.com>
2021-01-21 17:09:23 -0600

    Update p0.py
    first try idiot

--- p0.py
+++ p0.py
@@ -4,7 +4,7 @@
 # Anything outside of the starter/ and test/ folder is optional.
 
 
-def binarySearch(array, target):
+def binarySearch(A, k):
     """Apply binary search on array with the target number.
 
     Args:
@@ -15,12 +15,30 @@ def binarySearch(array, target):
         Index of target number in array if find, otherwise, return -1.
     """
     
-    #TODO: Implement without using python's in-built function
-    return -2
+    #TODO: Implement without using python's in-built functiondef binary(A, k):
+    def bSearch(A, k, low, high):
+        if high == low:
+            return A[low] == k
+        mid = (low + high)//2
+        if A[mid] == k:
+            '''print('Binary key found at position ' + str(mid))'''
+            return mid
+        elif A[mid] > k:
+            if low == mid:
+                return -1
+            else:
+                return bSearch(A, k, low, mid-1)
+        else:
+            return bSearch(A, k, mid+1, high)
+    if len(A) == 0:
+        return -1
+    else:
+        return bSearch(A, k, 0, len(A)-1)
+
     
     
     
-def linearSearch(array, target):
+def linearSearch(A, k):
     """Apply linear search on array with the target number.
 
     Args:
@@ -31,5 +49,8 @@ def linearSearch(array, target):
         Index of target number in array if find, otherwise, return -1.
     """
 
-    #TODO: Implement without using python's in-built function        
-    return -2
+    #TODO: Implement without using python's in-built function
+    for i in range(len(A)):
+        if A[i] == k:
+            return i
+    return -1
