a7307be4ede0cf6e989a80f2e3af2b840ee7b43c
Autograde Server <autograde@autograde.local>
2021-01-21 15:36:15 +0000

    Initial Starter Generated

---
+++ p0.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+# Answer code for testing purposes. Allows you to make use of the test/ folder.
+# Anything outside of the starter/ and test/ folder is optional.
+
+
+def binarySearch(array, target):
+    """Apply binary search on array with the target number.
+
+    Args:
+        array: An array of integers in non-decreasing order.
+        target: A int value which is target number.
+
+    Return:
+        Index of target number in array if find, otherwise, return -1.
+    """
+    
+    #TODO: Implement without using python's in-built function
+    return -2
+    
+    
+    
+def linearSearch(array, target):
+    """Apply linear search on array with the target number.
+
+    Args:
+        array: An array of integers in non-decreasing order.
+        target: A int value which is target number.
+
+    Return:
+        Index of target number in array if find, otherwise, return -1.
+    """
+
+    #TODO: Implement without using python's in-built function        
+    return -2
